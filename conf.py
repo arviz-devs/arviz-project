18b587967395df3d9b192a4c54d776fb84257c6f
Seth Axen <seth.axen@gmail.com>
2022-03-31 22:43:20 +0200

    Point to logo directory for logos

--- conf.py
+++ conf.py
@@ -32,14 +32,14 @@ myst_enable_extensions = ["colon_fence", "deflist", "dollarmath", "amsmath"]
 
 html_theme = "furo"
 html_title = project
-html_static_path = ["sphinx"]
+html_static_path = ["sphinx", "arviz_logos"]
 html_css_files = [
     "custom.css",
     "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/fontawesome.min.css",
 ]
 html_js_files = ["https://code.iconify.design/2/2.1.1/iconify.min.js"]
 logo = "ArviZ.png"
-html_favicon = "sphinx/favicon.ico"
+html_favicon = "arviz_logos/favicon.ico"
 html_theme_options = {
     "light_css_variables": {
         "color-brand-primary": "#0f718e",
@@ -51,7 +51,7 @@ html_theme_options = {
     },
     "sidebar_hide_name": True,
     "light_logo": logo,
-    "dark_logo": f"dark-{logo}",
+    "dark_logo": "ArviZ_white.png",
 }
 
 intersphinx_mapping = {"arviz": ("https://arviz-devs.github.io/arviz/", None)}
